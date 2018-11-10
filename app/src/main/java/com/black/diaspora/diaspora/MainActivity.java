9bce92e8301e1628b3a4eefa3c91fd12323f1aa8
Unknown <arthur.gomes_4718@hotmail.com>
2018-11-10 21:08:24 -0200

    Login activity Layout

--- app/src/main/java/com/black/diaspora/diaspora/MainActivity.java
+++ app/src/main/java/com/black/diaspora/diaspora/MainActivity.java
@@ -1,17 +1,35 @@
 package com.black.diaspora.diaspora;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.black.diaspora.diaspora.Views.Login;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageView splash;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        splash = (ImageView) findViewById(R.id.splashCreen);
+
+        splash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginScreen = new Intent(getApplicationContext(), Login.class);
+                startActivity(loginScreen);
+            }
+        });
     }
 
     //TODO load user token here
+
+
+    //TODO load any useful resource for the app in this splash screen
 
 
 }
